96efe503931e263e5003e38c1c7f32f77c93af45
Sergi Fernández <sergi.fernandez@eyeos.com>
2014-07-17 19:06:11 +0200

    solving problem with resize and overlap iframes

--- src/ventus/wm/window.js
+++ src/ventus/wm/window.js
@@ -14,8 +14,9 @@ define([
 function(Emitter, View, WindowTemplate, Resizer, MoverLimiter) {
 	'use strict';
 
-	var Window = function (options) {
+	var Window = function (options, manager) {
 		this.signals = new Emitter();
+		this.manager = manager;
 
 		options = options || {
 			title: 'Untitle Window',
@@ -63,6 +64,8 @@ function(Emitter, View, WindowTemplate, Resizer, MoverLimiter) {
 		this.$content = this.el.find('.wm-content');
 		if(options.content)
 			this.$content.append(options.content);
+
+		this.$content.append('<iframe type="text/html" src="//www.youtube.com/embed/8eb4tWf7_1Y" frameborder="0" allowfullscreen></iframe>');
 
 		// Cache header element
 		this.$titlebar = this.el.find('header');
@@ -114,7 +117,23 @@ function(Emitter, View, WindowTemplate, Resizer, MoverLimiter) {
 			}
 		},
 
+		doRenderOverlay: function() {
+			this.el.prepend('<div class="resize-overlay"></div>');
+		},
+
+		doEraseOverlay: function() {
+			$('.resize-overlay').remove();
+		},
+
+		addDivOverlay: function() {
+			this.manager.addOverlaysToAllWindows();
+		},
+		removeDivOverlay: function() {
+			this.manager.removeOverlaysToAllWindows();
+		},
+
 		events: {
+
 			window: {
 				'click': function(e) {
 					this.signals.emit('select', this, e);
@@ -133,7 +152,7 @@ function(Emitter, View, WindowTemplate, Resizer, MoverLimiter) {
 				},
 
 				'.wm-window-title mousedown': function(e) {
-					this.el.prepend('<div class="resize-overlay"></div>');
+					this.addDivOverlay();
 					this.slots.move.call(this, e);
 				},
 
@@ -181,7 +200,7 @@ function(Emitter, View, WindowTemplate, Resizer, MoverLimiter) {
 					};
 
 					this.el.addClass('resizing');
-					this.el.prepend('<div class="resize-overlay"></div>');
+					this.addDivOverlay();
 					
 
 					e.preventDefault();
@@ -203,7 +222,7 @@ function(Emitter, View, WindowTemplate, Resizer, MoverLimiter) {
 					this._moving['top-right'] = true;
 
 					this.el.addClass('resizing');
-					this.el.prepend('<div class="resize-overlay"></div>');
+					this.addDivOverlay();
 
 					e.preventDefault();
 				},
@@ -224,7 +243,7 @@ function(Emitter, View, WindowTemplate, Resizer, MoverLimiter) {
 					this._moving['top-left'] = true;
 
 					this.el.addClass('resizing');
-					this.el.prepend('<div class="resize-overlay"></div>');
+					this.addDivOverlay();
 
 					e.preventDefault();
 				},
@@ -245,7 +264,7 @@ function(Emitter, View, WindowTemplate, Resizer, MoverLimiter) {
 					this._moving['bottom-left'] = true;
 
 					this.el.addClass('resizing');
-					this.el.prepend('<div class="resize-overlay"></div>');
+					this.addDivOverlay();
 
 					e.preventDefault();
 				},
@@ -265,7 +284,7 @@ function(Emitter, View, WindowTemplate, Resizer, MoverLimiter) {
 					});
 
 					this.el.addClass('resizing');
-					this.el.prepend('<div class="resize-overlay"></div>');
+					this.addDivOverlay();
 
 					e.preventDefault();
 				},
@@ -285,7 +304,7 @@ function(Emitter, View, WindowTemplate, Resizer, MoverLimiter) {
 					});
 
 					this.el.addClass('resizing');
-					this.el.prepend('<div class="resize-overlay"></div>');
+					this.addDivOverlay();
 
 					e.preventDefault();
 				},
@@ -298,7 +317,7 @@ function(Emitter, View, WindowTemplate, Resizer, MoverLimiter) {
 						height: this.height - e.originalEvent.pageY
 					};
 					this.el.addClass('resizing');
-					this.el.prepend('<div class="resize-overlay"></div>');
+					this.addDivOverlay();
 
 					e.preventDefault();
 				},
@@ -312,7 +331,7 @@ function(Emitter, View, WindowTemplate, Resizer, MoverLimiter) {
 						height: this.height
 					};
 					this.el.addClass('resizing');
-					this.el.prepend('<div class="resize-overlay"></div>');
+					this.addDivOverlay();
 
 					e.preventDefault();
 				}
@@ -412,7 +431,7 @@ function(Emitter, View, WindowTemplate, Resizer, MoverLimiter) {
 						this._resizer = null;
 					}
 
-					$('.resize-overlay').remove();
+					this.removeDivOverlay();
 				}
 			}
 		},
