94fde679a3cf49778fedf82fd91a8f873776f1e7
jordins <jordi.nistal@gmail.com>
2014-10-31 12:41:20 +0100

    Generated build

--- build/ventus.js
+++ build/ventus.js
@@ -1444,6 +1444,7 @@ function(Emitter, View, WindowTemplate, Resizer, MoverLimiter) {
 
 					// Remove element
 					this.$content.html('');
+					this.el.remove();
 					this.signals.emit('closeDone', this);
 				}, this);
 			}
