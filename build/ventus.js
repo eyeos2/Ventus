f99a0c1635dbb2564c8147c870093d04cf97f533
Jenny Magtibay <jenifer.magtibay@eyeos.org>
2014-11-26 12:24:49 +0100

    Added build files

--- build/ventus.js
+++ build/ventus.js
@@ -1648,8 +1648,6 @@ function(Emitter, View, WindowTemplate, Resizer, MoverLimiter, MoverContainer, W
 						this.signals.emit('resize', this);
 					}
 
-					this.signals.emit('move', this);
-
 					this.removeDivOverlay();
 					e.stopPropagation();
 				}
